f7e7d8dd6486b52e6ac2ec637a97fef908ae8600
eo1989 <eorlowski6@gmail.com>
2023-06-13 14:04:05 -0400

    asdfasdf

---
+++ trialing_some_broken_funcs_debug_liekly.py
@@ -0,0 +1,15 @@
+from openbb_terminal.sdk import openbb
+openbb.stocks.candle("SPY", use_matplotlib=False)
+
+# from ipywidgets import interact
+# import numpy as np
+# import plotly.graph_objects as go
+
+# @interact
+# def foo(beta = (-10, 10, 1)):
+#     my_x = np.arange(0, 100, 1)
+#     my_y = my_x * beta
+#     print(f"debug: {my_y}")
+#     fig = go.FigureWidget()
+#     fig.add_scatter(x = my_x, y = my_y)
+#     fig.show()
